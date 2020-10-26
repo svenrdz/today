89ebcc470e23f2125bb7920192909faf6ec118f9
sven <svenbmr@gmail.com>
2020-10-26 11:24:16 +0100

    feat: add revert fun & `dateof` command

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="today",
-    version="0.1",
+    version="0.2",
     description="today's date",
     author="Sven Rodriguez",
     author_email="svenbmr@gmail.com",
     packages=find_packages(),
-    entry_points={"console_scripts": ["today = today.today:main"]},
+    entry_points={
+        "console_scripts": [
+            "today = today.today:today",
+            "dateof = today.today:dateof",
+        ]
+    },
 )
--- today/today.py
+++ today/today.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+import sys
 import datetime
+
+
+def bisextile(year):
+    return year % 4 == 0
 
 
 def convert(date_or_year, month=None, day=None):
@@ -8,6 +13,8 @@ def convert(date_or_year, month=None, day=None):
     else:
         date = date_or_year
     days = date - datetime.datetime(date.year - 1, 12, 31)
+    if days.days > 60 and bisextile(date.year):
+        days -= datetime.timedelta(days=1)
     month = days.days // 10
     if month >= 10:
         month = chr(ord("a") + month - 10)
@@ -15,9 +22,25 @@ def convert(date_or_year, month=None, day=None):
     return "".join(map(str, [date.year % 100, month, day]))
 
 
-def main():
+def revert(todate: str):
+    assert isinstance(todate, str)
+    todate = todate.strip()
+    assert len(todate) == 4
+    year = int(todate[:2]) + 2000
+    try:
+        tens = int(todate[2])
+    except ValueError:
+        tens = ord(todate[2]) - ord("a") + 10
+    days = 10 * tens + int(todate[3])
+    if days > 59 and bisextile(year):
+        days += 1
+    date = datetime.datetime(year - 1, 12, 31) + datetime.timedelta(days=days)
+    return date.strftime("%d, %b %Y")
+
+
+def today():
     print(convert(datetime.datetime.today()))
 
 
-if __name__ == "__main__":
-    main()
+def dateof():
+    print(revert(sys.argv[1]))
